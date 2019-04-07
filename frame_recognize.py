4092294397d8c94cbf0aa151a8827d2726e90ac1
Ganesh <ganesh.nanduru@gmail.com>
2019-04-06 22:28:09 -0400

    fast detection and crop

--- frame_recognize.py
+++ frame_recognize.py
@@ -1,18 +1,16 @@
 import face_recognition
-import cv2
-import imutils
 
-img1 = cv2.imread("C:/Users/ganes/Pictures/facerec_pics/ganesh/0.jpg")
-img2 = cv2.imread("test/ganesh.jpg")
-img1 = imutils.resize(img1, width=500)
-cv2.imshow("sample", img1)
-cv2.imshow("test", img2)
-cv2.waitKey(10000)
-
-known_image = face_recognition.load_image_file("C:/Users/ganes/Pictures/facerec_pics/ganesh/0.jpg")
-unknown_image = face_recognition.load_image_file("test/ganesh.jpg")
+known_image = face_recognition.load_image_file("test/joe-biden.jpg")
+unknown_image = face_recognition.load_image_file("test/joe-biden2.jpg")
+new_image = face_recognition.load_image_file("test/asian.png")
+#known_face_locations=[(0, width, height, 0)]
 
 biden_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+new_encoding = face_recognition.api.face_encodings(new_image, known_face_locations=[(0,182,182,0)], num_jitters=1)
 
 results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+results2 = face_recognition.compare_faces([new_encoding], unknown_encoding)
+
+print(results)
+print(results2)
--- sort_dataset.py
+++ sort_dataset.py
@@ -1,6 +1,6 @@
 import os
 
-directory = "C:/Users/ganes/Pictures/facerec_pics/ganesh/"
+directory = "C:/Users/ganes/Pictures/facerec_pics/aryan/"
 i = 1;
 for filename in os.listdir(directory):
     os.rename(directory+filename,directory+str(i)+".jpg")
