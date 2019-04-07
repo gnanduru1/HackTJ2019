47f2a0ecfcc4d1eb6d1c84871015da60eb010957
Srikar Gouru <srikarg89@gmail.com>
2019-04-06 23:09:32 -0400

    Everything

--- sort_dataset.py
+++ sort_dataset.py
@@ -2,7 +2,7 @@ import os
 
 loops = int(input("Number of folders? "))
 for x in range(loops):
-    directory = "C:/Users/ganes/Pictures/facerec_pics/"+ input("Folder name? ") +"/"
+    directory = "C:/Users/Srikar/Documents/Hackathons/facerec_testdata/"+ input("Folder name? ") +"/"
     i = 1;
     for filename in os.listdir(directory):
         os.rename(directory+filename,directory+str(i)+".jpg")
